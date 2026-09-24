fc9b90961e434a2485be9ddb51e4be7cf1ce5699
MichailDev <76217159+MichailDev@users.noreply.github.com>
2026-09-24 09:07:39 +0300

    Delete ios directory

--- ios/SyndicateQuant/SyndicateQuant/SyndicateQuantApp.swift
+++
@@ -1,8 +0,0 @@
-import SwiftUI
-import SwiftData
-
-@main struct SyndicateQuantApp: App {
-    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
-    @StateObject private var settings=AppSettings()
-    var body: some Scene { WindowGroup { RootView().environmentObject(settings) }.modelContainer(for:[JournalEntry.self,CalibrationSample.self,BacktestRun.self]) }
-}
